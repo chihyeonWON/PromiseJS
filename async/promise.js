96e0b806cef40717ed7956f55cecb01ebbaf09c8
원치현 <heungeob1003@gmail.com>
2021-11-14 14:06:30 +0900

    Promise Producer 생성

---
+++ async/promise.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Promise is a Javascript object for asynchronous operation.
+// state: pending -> fulfilled or rejected
+// Producer vs Consumer
+
+// 1. Producer
+// when new Promise is created, the executor runs automatically.
+const promise = new Promise((resolve, reject) => {
+    // doing some heavy work (network, read files)
+    console.log('doing something...');
+    setTimeout(() => {
+        resolve('ellie');
+    }, 2000)
+});
